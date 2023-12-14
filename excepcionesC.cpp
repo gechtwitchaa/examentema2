808fe4454e8e5e40f64e383c978f14053d6c54d5
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-14 13:02:51 +0100

    .
    funcion para mostrar mensajes de error

--- excepcionesC.cpp
+++ excepcionesC.cpp
@@ -11,3 +11,18 @@ typedef enum {
     FORMATO_FECHA_INCORRECTO,
     MATERIA_NO_REGISTRADA
 } Excepcion;
+
+
+void manejarExcepcion(Excepcion excepcion) {
+    switch (excepcion) {
+        case FORMATO_FECHA_INCORRECTO:
+            printf("Error: Formato de fecha incorrecto\n");
+            break;
+        case MATERIA_NO_REGISTRADA:
+            printf("Error: Materia no registrada\n");
+            break;
+        default:
+            printf("Error desconocido\n");
+            break;
+    }
+}
