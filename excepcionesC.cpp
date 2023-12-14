dc2f3a94a656062d6e840b754aa4927f717baa71
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-14 13:04:44 +0100

    funcion para mostrar la asistencia del estudiante y funcion para registrar asistencia del estudiante utilizando excepciones

--- excepcionesC.cpp
+++ excepcionesC.cpp
@@ -42,3 +42,66 @@ struct Estudiante {
     struct Asistencia asistencias[MAX_ASISTENCIAS]; // Array de asistencias
     int cantidadAsistencias;
 };
+
+
+void mostrarAsistencia(const struct Estudiante estudiante) {
+    printf("Asistencias de %s:\n", estudiante.nombre);
+    for (int i = 0; i < estudiante.cantidadAsistencias; ++i) {
+        printf("Fecha: %s | Materia: %s | Estado: %s\n", estudiante.asistencias[i].fecha,
+               estudiante.asistencias[i].materia, estudiante.asistencias[i].estado);
+    }
+    printf("\n");
+}
+
+
+void registrarAsistencia(struct Estudiante *estudiante, const char *fecha, const char *materia, const char *estado) {
+    // Verificar formato de fecha
+    if (strlen(fecha) != 10 || fecha[4] != '-' || fecha[7] != '-') {
+        manejarExcepcion(FORMATO_FECHA_INCORRECTO);
+        return;
+    }
+
+
+    char materiasRegistradas[][MAX_MATERIA] = {"Matemáticas", "Física", "Programación"};
+    int numMateriasRegistradas = sizeof(materiasRegistradas) / sizeof(materiasRegistradas[0]);
+    int materiaRegistrada = 0;
+    for (int i = 0; i < numMateriasRegistradas; ++i) {
+        if (strcmp(materiasRegistradas[i], materia) == 0) {
+            materiaRegistrada = 1;
+            break;
+        }
+    }
+    if (!materiaRegistrada) {
+        manejarExcepcion(MATERIA_NO_REGISTRADA);
+        return;
+    }
+
+    if (estudiante->cantidadAsistencias < MAX_ASISTENCIAS) {
+        strcpy(estudiante->asistencias[estudiante->cantidadAsistencias].fecha, fecha);
+        strcpy(estudiante->asistencias[estudiante->cantidadAsistencias].materia, materia);
+        strcpy(estudiante->asistencias[estudiante->cantidadAsistencias].estado, estado);
+        estudiante->cantidadAsistencias++;
+    } else {
+        printf("No se pueden registrar más asistencias\n");
+    }
+}
+
+int main() {
+    // Crear un objeto de tipo Estudiante
+    struct Estudiante estudiante1;
+
+    // Asignar valores a los campos del estudiante
+    strcpy(estudiante1.nombre, "Alberto");
+    estudiante1.edad = 18;
+    estudiante1.promedio = 8;
+    estudiante1.cantidadAsistencias = 0;
+
+    // Registrar asistencias del estudiante con manejo de excepciones simuladas
+    registrarAsistencia(&estudiante1, "2023-12-01", "Matemáticas", "asistió");
+    registrarAsistencia(&estudiante1, "23-12-2023", "Historia", "asistió"); // Formato de fecha incorrecto
+
+    // Mostrar la asistencia del estudiante
+    mostrarAsistencia(estudiante1);
+
+    return 0;
+}
