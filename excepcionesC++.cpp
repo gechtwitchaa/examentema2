50b3b41d5d8ad410bc30b2f76ea95bef108089f4
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-14 12:57:28 +0100

    funcion para mostrar asistencia y para registrar asistencia con excepciones

--- excepcionesC++.cpp
+++ excepcionesC++.cpp
@@ -36,3 +36,29 @@ struct Estudiante {
     float promedio;
     vector<Asistencia> asistencias; // Vector de asistencias
 };
+
+
+void mostrarAsistencia(const Estudiante& estudiante) {
+    cout << "Asistencias de " << estudiante.nombre << ":" << endl;
+    for (const auto& asistencia : estudiante.asistencias) {
+        cout << "Fecha: " << asistencia.fecha << " | Materia: " << asistencia.materia << " | Estado: " << asistencia.estado << endl;
+    }
+    cout << endl;
+}
+
+
+void registrarAsistencia(Estudiante& estudiante, const string& fecha, const string& materia, const string& estado) {
+
+    if (fecha.size() != 10 || fecha[4] != '-' || fecha[7] != '-') {
+        throw FormatoFechaIncorrecto();
+    }
+
+
+    vector<string> materiasRegistradas = {"Matemáticas", "Física", "Programación"};
+    if (find(materiasRegistradas.begin(), materiasRegistradas.end(), materia) == materiasRegistradas.end()) {
+        throw MateriaNoRegistrada();
+    }
+
+    Asistencia nuevaAsistencia = {fecha, materia, estado};
+    estudiante.asistencias.push_back(nuevaAsistencia);
+}
