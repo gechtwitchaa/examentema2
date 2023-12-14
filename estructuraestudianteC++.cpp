782f70b183dcb55568dd5f460b6dfddc99dd736b
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-14 12:01:58 +0100

    .
    campos de la estructura estudiante

--- estructuraestudianteC++.cpp
+++ estructuraestudianteC++.cpp
@@ -3,9 +3,16 @@
 
 using namespace std;
 
-// Definición de la estructura Estudiante
+
 struct Estudiante {
     string nombre;
     int edad;
     float promedio;
 };
+
+
+void mostrarEstudiante(const Estudiante& estudiante) {
+    cout << "Nombre: " << estudiante.nombre << endl;
+    cout << "Edad: " << estudiante.edad << endl;
+    cout << "Promedio: " << estudiante.promedio << endl;
+}
