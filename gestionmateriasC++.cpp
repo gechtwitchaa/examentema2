b8f45bc982f0a51d622435f78b50c64f1beb961d
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-14 12:24:41 +0100

    .
    definicion de la estructura y muestra de datos como en el codigo anterior

---
+++ gestionmateriasC++.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+struct Estudiante {
+    string nombre;
+    int edad;
+    float promedio;
+    vector<string> materias; // Vector de materias
+};
+
+
+void mostrarEstudiante(const Estudiante& estudiante) {
+    cout << "Nombre: " << estudiante.nombre << endl;
+    cout << "Edad: " << estudiante.edad << endl;
+    cout << "Promedio: " << estudiante.promedio << endl;
+
+    cout << "Materias inscritas:" << endl;
+    for (const auto& materia : estudiante.materias) {
+        cout << "- " << materia << endl;
+    }
+    cout << endl;
+}
